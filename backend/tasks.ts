daa90a41fd5d8cb3f05ac1ab4ba118277852352c
xeureka <teklemariameureka@gmail.com>
2025-07-30 06:22:24 +1000

    .

--- backend/tasks.ts
+++
@@ -1,7 +0,0 @@
-/**
- * have a regined work plan
- * check out and underrstand the very fayda
- * then work the logivs from there 
- */
-
-// Nigga am lost tey got the most hard to understand and poorly documented system besmeam
