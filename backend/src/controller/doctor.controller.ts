70b56f215e056bdeed539c2c433103e57c134a55
xeureka <teklemariameureka@gmail.com>
2025-07-30 21:02:51 +1000

    fix: register, login and logout api end point workout done

--- backend/src/controller/doctor.controller.ts
+++ backend/src/controller/doctor.controller.ts
@@ -22,7 +22,7 @@ export const getPatientData = async (req: Request, res: Response) => {
 
 export const addNotesToPatient = async (req: Request, res: Response) => {
   try {
-    const updatedPatient = await doctorService.addNotes(req.params.id, req.body.note,'12345678');
+    const updatedPatient = await doctorService.addNotes(req.params.id, req.body.note,'6889d0f6b7ff4fe17487d0f2');
     res.json({ message: "Note added", patient: updatedPatient });
   } catch (err: any) {
     res.status(500).json({ message: err.message });
