557234fe4634b50d3178eba0ffbc9106ba4e9ea7
andi-gech <andi.fab23@gmail.com>
2025-07-30 05:06:26 -0700

    oidc fixed

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -8,25 +8,28 @@ export const exchangeCodeForToken = async (req: Request, res: Response) => {
   try {
     console.log("Exchanging code for token with body:", req.body);
     const { code } = req.body;
-    const { TOKEN_ENDPOINT, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = getOIDCConfig();
-console.log("Using OIDC Config:", { TOKEN_ENDPOINT, CLIENT_ID, REDIRECT_URI });
-const jwt = await generateSignedJwt();    
-const response = await axios.post(
-      TOKEN_ENDPOINT,
-      new URLSearchParams({
+    const { TOKEN_ENDPOINT, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI , CLIENT_ASSERTION_TYPE} = getOIDCConfig();
+console.log("Using OIDC Config:", { TOKEN_ENDPOINT, CLIENT_ID, REDIRECT_URI, CLIENT_ASSERTION_TYPE });
+const jwt = await generateSignedJwt();  
+const params= new URLSearchParams({
 
         grant_type: "authorization_code",
         code,
         redirect_uri: REDIRECT_URI,
         client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
+     
+        client_assertion_type: CLIENT_ASSERTION_TYPE ,
         client_assertion: jwt,
+         code_verifier: 'dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk'
         
-      }),
+      }) 
+const response = await axios.post(
+      TOKEN_ENDPOINT,
+      params.toString(),
       { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
     );
 console.log("Token exchange response:", response.data);
-    return res.json(response.data);
+   res.json({ access_token: response.data.access_token });
   } catch (error: any) {
     console.error("Token exchange error:", error.response?.data || error.message);
     res.status(500).json({ message: "Failed to exchange token" });
@@ -37,7 +40,8 @@ export const fetchUserInfo = async (req: Request, res: Response) => {
   try {
     const { access_token } = req.body;
     const { USERINFO_ENDPOINT } = getOIDCConfig();
-
+console.log("Fetching user info with access token:", access_token);
+console.log("Using OIDC Config:", { USERINFO_ENDPOINT });
     const response = await axios.get(USERINFO_ENDPOINT, {
       headers: { Authorization: `Bearer ${access_token}` },
     });
